d79359110878578e888366bc80b9f139ae49fdc3
Jeff Dickey <dickeyxxx@gmail.com>
2016-05-04 15:50:43 -0700

    testing

--- test.js
+++ test.js
@@ -1,7 +1,59 @@
 'use strict'
 
-const {test} = require('ava')
+const ava = require('ava')
+const test = ava.test
+const Heroku = require('.')
+const heroku = new Heroku()
+const nock = require('nock')
 
-test('request', t => {
-  t.pass()
+test.before(() => {
+  nock.disableNetConnect()
+})
+
+test('get /apps', t => {
+  let api = nock('https://api.heroku.com')
+  .get('/apps')
+  .reply(200, [{name: 'myapp'}])
+
+  return heroku.get('/apps')
+  .then(apps => {
+    t.is(apps[0].name, 'myapp')
+  })
+  .then(() => api.done())
+})
+
+test('post /apps', t => {
+  let api = nock('https://api.heroku.com')
+  .post('/apps', {name: 'myapp'})
+  .reply(201)
+
+  return heroku.post('/apps', {body: {name: 'myapp'}})
+  .then(() => api.done())
+})
+
+test('delete /apps', t => {
+  let api = nock('https://api.heroku.com')
+  .delete('/apps', {name: 'myapp'})
+  .reply(201)
+
+  return heroku.delete('/apps', {body: {name: 'myapp'}})
+  .then(() => api.done())
+})
+
+test('patch /apps', t => {
+  let api = nock('https://api.heroku.com')
+  .patch('/apps', {name: 'myapp'})
+  .reply(201)
+
+  return heroku.patch('/apps', {body: {name: 'myapp'}})
+  .then(() => api.done())
+})
+
+test('put /apps', t => {
+  let api = nock('https://api.heroku.com')
+  .put('/apps', {name: 'myapp'})
+  .reply(201)
+
+  return heroku.put('/apps', {body: {name: 'myapp'}})
+  .then(() => api.done())
 })
