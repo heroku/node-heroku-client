8715113a1b36faa2ea8d6ca3646ce6a5a73991ae
Katy Bowman <katy.bowman@salesforce.com>
2025-10-09 11:46:49 -0400

    chore: update to the latest version of standard and fix issues

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const test = require('ava')
-// const test = ava.test
 const Heroku = require('.')
 const heroku = new Heroku()
 const nock = require('nock')
@@ -12,75 +11,75 @@ test.before(() => {
 })
 
 test('get /apps', t => {
-  let api = nock('https://api.heroku.com')
-  .get('/apps')
-  .reply(200, [{name: 'myapp'}])
+  const api = nock('https://api.heroku.com')
+    .get('/apps')
+    .reply(200, [{ name: 'myapp' }])
 
   return heroku.get('/apps')
-  .then(apps => {
-    t.is(apps[0].name, 'myapp')
-  })
-  .then(() => api.done())
+    .then(apps => {
+      t.is(apps[0].name, 'myapp')
+    })
+    .then(() => api.done())
 })
 
 test('post /apps', t => {
-  let api = nock('https://api.heroku.com')
-  .post('/apps', {name: 'myapp'})
-  .reply(201)
+  const api = nock('https://api.heroku.com')
+    .post('/apps', { name: 'myapp' })
+    .reply(201)
 
-  return heroku.post('/apps', {body: {name: 'myapp'}})
-  .then(apps => {
-    t.deepEqual(apps, {})
-  })
-  .then(() => api.done())
+  return heroku.post('/apps', { body: { name: 'myapp' } })
+    .then(apps => {
+      t.deepEqual(apps, {})
+    })
+    .then(() => api.done())
 })
 
 test('delete /apps', t => {
-  let api = nock('https://api.heroku.com')
-  .delete('/apps', {name: 'myapp'})
-  .reply(201)
+  const api = nock('https://api.heroku.com')
+    .delete('/apps', { name: 'myapp' })
+    .reply(201)
 
-  return heroku.delete('/apps', {body: {name: 'myapp'}})
-  .then(apps => {
-    t.deepEqual(apps, {})
-  })
-  .then(() => api.done())
+  return heroku.delete('/apps', { body: { name: 'myapp' } })
+    .then(apps => {
+      t.deepEqual(apps, {})
+    })
+    .then(() => api.done())
 })
 
 test('patch /apps', t => {
-  let api = nock('https://api.heroku.com')
-  .patch('/apps', {name: 'myapp'})
-  .reply(201)
+  const api = nock('https://api.heroku.com')
+    .patch('/apps', { name: 'myapp' })
+    .reply(201)
 
-  return heroku.patch('/apps', {body: {name: 'myapp'}})
-  .then(apps => {
-    t.deepEqual(apps, {})
-  })
-  .then(() => api.done())
+  return heroku.patch('/apps', { body: { name: 'myapp' } })
+    .then(apps => {
+      t.deepEqual(apps, {})
+    })
+    .then(() => api.done())
 })
 
 test('put /apps', t => {
-  let api = nock('https://api.heroku.com')
-  .put('/apps', {name: 'myapp'})
-  .reply(201)
+  const api = nock('https://api.heroku.com')
+    .put('/apps', { name: 'myapp' })
+    .reply(201)
 
-  return heroku.put('/apps', {body: {name: 'myapp'}})
-  .then(apps => {
-    t.deepEqual(apps, {})
-  })
-  .then(() => api.done())
+  return heroku.put('/apps', { body: { name: 'myapp' } })
+    .then(apps => {
+      t.deepEqual(apps, {})
+    })
+    .then(() => api.done())
 })
 
 test('non-http', t => {
-  let api = nock('http://api.heroku.com')
-  .get('/apps')
-  .reply(200, [{name: 'myapp'}])
+  const api = nock('http://api.heroku.com')
+    .get('/apps')
+    .reply(200, [{ name: 'myapp' }])
 
-  return heroku.get('/apps', {host: 'http://api.heroku.com'})
-  .then(apps => {
-    t.is(apps[0].name, 'myapp')
-  })
-  .then(() => api.done())
+  return heroku.get('/apps', { host: 'http://api.heroku.com' })
+    .then(apps => {
+      t.is(apps[0].name, 'myapp')
+    })
+    .then(() => api.done())
 })
 
 test('url: https', t => {
@@ -100,7 +99,7 @@ test('request does not produce any stderr', async t => {
 
   stdMock.stderr.start()
 
-  let api = nock('https://api.heroku.com')
+  const api = nock('https://api.heroku.com')
     .get('/apps')
     .reply(200, [])
 
